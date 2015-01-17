0474a71d2a4d0d820a3eacb40d4e41b0cd871800
Waiyaki <muturijames3@gmail.com>
2015-01-18 01:24:21 +0300

    Through with Chapter 6 - Models and Databases

--- tango_with_django/rango/urls.py
+++ tango_with_django/rango/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 from rango import views
 
-urlpatterns = patterns('', 
+urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
 )
--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
+
 def index(request):
     return render(request, 'rango/index.html', {})
+
 
 def about(request):
     return render(request, 'rango/about.html', {})
